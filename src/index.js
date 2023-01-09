29b78cdddda7a878b9a720579e56ce4bf34704a7
oscar.cardenas <oscar.cardenas@gse.com.co>
2023-01-09 18:34:35 -0500

    redux persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
+
+let persistore = persistStore(store);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistore}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
